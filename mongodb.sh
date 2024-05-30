af6c65c37d11756a48a73d80c38fc5558553d6d6
rakesh <rakeshreddy@gmail.com>
2024-05-30 22:27:07 +0530

    modified mongo.repo and mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -6,29 +6,31 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-validate () 
-    if [ $1 -ne 0 ]
-    then
-    echo "$2.. failure"
-    else
-    echo "$2.. success
-    fi
-    
 id=$(id -u)
 if [ $id -ne 0 ]
 then
 echo -e "$R user is not root $N. $G kindly proceed with root access$N"
 exit 1
 fi
-yum install mongodb-org -y
-validate $? "installing mongodb"
-systemctl enable mongod
-validate $? "enabling mongodb"
-systemctl start mongod
+validate() {
+    if [ $1 -ne 0 ]
+    then
+    echo "$2.. failure"
+    else
+    echo "$2.. success"
+    fi
+}
+cp mongo.repo /etc/yum.repos.d/mongo.repo $>> $logfile
+validate $? "copying mongo.repo"
+yum install mongodb-org -y $>> $logfile
+validate $? "installing mongodb" 
+systemctl enable mongod $>> $logfile
+validate $? "enabling mongodb" 
+systemctl start mongod $>> $logfile
 validate $? "start mongodb"
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf $>> $logfile
 validate $? "changing ip address mongodb"
-systemctl restart mongod
+systemctl restart mongod $>> $logfile
 validate $? "restarted mongod"
 
 
