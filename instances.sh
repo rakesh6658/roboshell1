a7428305cb4aacc8d17d57cba188cf8f160af410
rakesh <rakeshreddy@gmail.com>
2024-06-05 16:49:20 +0530

    added instances.sh

--- instances.sh
+++ instances.sh
@@ -6,7 +6,7 @@ subnetid=subnet-036554fe159737dc4
 modules=( "mongodb" "redis" "catalogue" "cart" "user" "mysql" "rabbitmq" "shipping" "dispatch" "payment" )
 for i in $modules
 do
-if [ ($i=="mongodb" || $i=="mysql")  ]
+if [ (i=="mongodb" || i=="mysql")  ]
 then
 $instance_type=t3.micro
 private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --subnet-id $subnetid | jq -r '.Instances[0].PrivateIpAddress')
