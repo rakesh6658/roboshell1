603e58305cf2b3c7be0333408490a19f18de82dc
rakesh <rakeshreddy@gmail.com>
2024-06-05 16:38:54 +0530

    added instances.sh

--- instances.sh
+++ instances.sh
@@ -13,25 +13,4 @@ private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $insta
 else
 private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --subnet-id $subnetid | jq -r '.Instances[0].PrivateIpAddress')
 fi
-aws route53 change-resource-record-sets --hosted-zone-id Z07861153FFB7P0M0D6G8 --change-batch 
-{
-    "Comment": "Update record to add new CNAME record",
-    "Changes": 
-    [
-        {
-            "Action": "CREATE",
-            "ResourceRecordSet": {
-                "Name": "$modules.rakeshreddy.online",
-                "Type": "A",
-                "TTL": "300"
-                "ResourceRecords": [
-                    {
-                        "Value": "$modules"
-                    }
-                ]
-            }
-        }
-    ]
-}
-
 done
