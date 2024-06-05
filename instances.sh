6bf7d805bf6692b7dde95e95e7e34c1d2fe92c31
rakesh <rakeshreddy@gmail.com>
2024-06-05 17:33:16 +0530

    added instances.sh

--- instances.sh
+++ instances.sh
@@ -5,14 +5,34 @@ subnetid=subnet-036554fe159737dc4
 
 
 modules=( "mongodb" "redis" "catalogue" "cart" "user" "mysql" "rabbitmq" "shipping" "dispatch" "payment" )
-echo "${modules[@]}"
-# for i in $modules$@
-# do
-# if ((i=="mongodb" || i=="mysql"))
-# then
-# instance_type=t3.micro
-# private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --subnet-id $subnetid | jq -r '.Instances[0].PrivateIpAddress')
-# else
-# private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --subnet-id $subnetid | jq -r '.Instances[0].PrivateIpAddress')
-# fi
-# done
+for i in "${modules[@]}"
+do
+if ((i=="mongodb" || i=="mysql"))
+then
+instance_type=t3.micro
+private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --subnet-id $subnetid | jq -r '.Instances[0].PrivateIpAddress')
+else
+private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --subnet-id $subnetid | jq -r '.Instances[0].PrivateIpAddress')
+fi
+done
+aws route53 change-resource-record-sets --hosted-zone-id Z07861153FFB7P0M0D6G8 --change-batch 
+{
+    "Comment": "Update record to add new CNAME record",
+    "Changes": 
+    [
+        {
+            "Action": "CREATE",
+            "ResourceRecordSet": {
+                "Name": "${modules[@]}.rakeshreddy.online",
+                "Type": "A",
+                "TTL": "300",
+                "ResourceRecords": [
+                    {
+                        "Value": "$modules"
+                    }
+                ]
+            }
+        }
+    ]
+}
+done
