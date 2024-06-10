7e1c33f944aa6ead9de6be1429afc61070d7868a
rakesh <rakeshreddy@gmail.com>
2024-06-10 19:41:22 +0530

    modified instances.sh

--- instances.sh
+++ instances.sh
@@ -1,6 +1,8 @@
 imageid=ami-0b4f379183e5706b9
 instance_type=""
 security_groupid=sg-0ba387cf504f1383b
+DOMAIN_NAME=rakeshreddy.online
+HOSTED_ZONE_ID=01861853FDUCS1FFDRY1
 for i in $@
 do
 if ((i=="mongodb" || i=="mysql"))
@@ -11,17 +13,16 @@ instance_type=t2.micro
 fi
 echo "creating instance $i"
 private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-
-
-aws route53 change-resource-record-sets --hosted-zone-id Z01861853FDUCS1FFDRY1 --change-batch'
-{
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
+    {
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
-                            "Name": "'$i.rakeshreddy.online'",
+                            "Name": "'$i.$DOMAIN_NAME'",
                             "Type": "A",
                             "TTL": 300,
                             "ResourceRecords": [{ "Value": "'$private_ip'"}]
                         }}]
-}'
+    }
+    '
 done
