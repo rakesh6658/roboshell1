7237a5cb5268fd02b0b3ce0c1201f9b873f632a8
rakesh <rakeshreddy@gmail.com>
2024-06-10 19:04:32 +0530

    modified instances.sh

--- instances.sh
+++ instances.sh
@@ -1,19 +1,17 @@
 imageid=ami-0f3c7d07486cad139
-instance_type=t2.micro
-security_groupid=sg-05767c7a9b8ae16b0
-subnetid=subnet-036554fe159737dc4
-
-
-modules=( "mongodb" "redis")
-for i in "${modules[@]}"
+instance_type=""
+security_groupid=sg-0ba387cf504f1383b
+for i in $@
 do
 if ((i=="mongodb" || i=="mysql"))
 then
 instance_type=t3.micro
-private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --subnet-id $subnetid | jq -r '.Instances[0].PrivateIpAddress')
 else
-private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --subnet-id $subnetid | jq -r '.Instances[0].PrivateIpAddress')
+instance_type=t2.micro
 fi
+creating instance $i
+private_ip=$( aws ec2 run-instances  --image-id $imageid  --instance-type $instance_type  --security-group-ids $security_groupid --subnet-id $subnetid | jq -r '.Instances[0].PrivateIpAddress')
+
 done
 # aws route53 change-resource-record-sets --hosted-zone-id Z07861153FFB7P0M0D6G8 --change-batch 
 #"mysql" "rabbitmq" "shipping" "dispatch" "payment"
@@ -29,7 +27,7 @@ done
 #                 "TTL": "300",
 #                 "ResourceRecords": [
 #                     {
-#                         "Value": "$modules"
+#                         "Value": "$private_ip"
 #                     }
 #                 ]
 #             }
